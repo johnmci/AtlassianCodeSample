0cd7c36c37ae59ffd9c58d712b384f6a5b42f6d1
johnmci <johnmci@smalltalkconsulting.com>
2016-05-01 12:40:57 -0700

    Initial Commit

---
+++ MattLangan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MattLangan
+//
+//  Created by John M McIntosh on 2016-05-01.
+//  Copyright © 2016 Corporate Smalltalk Consulting Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
